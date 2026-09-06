5ce76d6dca63f7e21e3470e781ceda80448523b8
jcm11215 <jcm11215@gmail.com>
2026-09-06 18:14:38 -0500

    Delete src directory

--- src/utils/collection.js
+++
@@ -1,7 +0,0 @@
-/** Generic collection helpers. */
-
-export function distinctValues(field, arr, extra){
-  const set = new Set((extra||[]));
-  arr.forEach(x=>{ if(x[field]) set.add(x[field]); });
-  return [...set].filter(v=>v && v!=='Unassigned').sort();
-}
--- src/utils/id.js
+++
@@ -1,3 +0,0 @@
-/** Opaque id generation. */
-
-export function uid(prefix){ return prefix + '_' + Date.now().toString(36) + Math.random().toString(36).slice(2,7); }
